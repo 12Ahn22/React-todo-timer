8b879b9c354c391865a87eed082e0a43bf5e59df
12ayo22 <12ayo22@gmail.com>
2023-01-10 14:27:50 +0900

    feat: router 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Main from './page/Main';
+import Time from './page/Time';
+import ToDo from './page/ToDo';
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route index element={<Main />} />
+      <Route path="time" element={<Time />} />
+      <Route path="todo" element={<ToDo />} />
+      {/* 없는 URL 접근 시, /로 이동 */}
+      <Route path="*" element={<Navigate to="/" replace={true} />} />
+    </Routes>
+  );
 }
 
 export default App;
