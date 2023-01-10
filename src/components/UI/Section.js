e153b81b20521fc49c7e9159f623c09ee0e3c616
12ayo22 <12ayo22@gmail.com>
2023-01-10 16:35:56 +0900

    fix: Section 높이 변경

--- src/components/UI/Section.js
+++ src/components/UI/Section.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 const MainContainer = styled.section`
   min-width: 22.5rem;
-  min-height: 40rem;
+  min-height: 100vh;
+  /* min-height: 40rem; */
   background: linear-gradient(
     180deg,
     #39388f 0%,
